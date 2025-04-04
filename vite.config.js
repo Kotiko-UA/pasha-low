9ae075f33bc93c5073049b8ddd18ab33baf41eb5
kotiko <megasuslik666@gmail.com>
2025-04-04 17:03:21 +0300

    add start files

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import glob from 'glob'
+import injectHTML from 'vite-plugin-html-inject'
+import FullReload from 'vite-plugin-full-reload'
+
+export default defineConfig({
+	root: 'src',
+	base: './',
+	build: {
+		rollupOptions: {
+			input: [
+				...glob.sync('./src/*.html'), // Усі HTML з кореневої папки
+			],
+		},
+		outDir: '../dist',
+	},
+	server: {
+		open: true,
+		host: '0.0.0.0',
+	},
+
+	plugins: [injectHTML(), FullReload(['./src/**/*'])],
+})
